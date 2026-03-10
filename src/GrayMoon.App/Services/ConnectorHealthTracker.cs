a0f754eec87f4ebacd31803ffcb8d19200ae9a58
Matt Janda <matt.janda.kingston@gmail.com>
2026-03-10 18:00:50 +0530

    A few more changes

--- src/GrayMoon.App/Services/ConnectorHealthTracker.cs
+++
@@ -1,8 +0,0 @@
-namespace GrayMoon.App.Services;
-
-/// <summary>Tracks connector health for UI (e.g. token decryption issues).</summary>
-public sealed class ConnectorHealthTracker
-{
-    public bool HasTokenDecryptionErrors { get; set; }
-}
-
