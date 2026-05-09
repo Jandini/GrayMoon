073689debcf5854aad40b343e7c211753f670b82
Matt Janda <matt.janda.kingston@gmail.com>
2026-05-09 18:11:16 +0530

    This is looking cool with terminus font

--- src/GrayMoon.App/wwwroot/js/loadingOverlayTerminal.js
+++ src/GrayMoon.App/wwwroot/js/loadingOverlayTerminal.js
@@ -1,6 +1,11 @@
 window.loadingOverlayTerminal = {
   scrollToEnd: (el) => {
     if (!el) return;
-    el.scrollTop = el.scrollHeight;
+    // Defer until after Blazor layout/paint so scrollHeight is final (avoids jump from wrong height).
+    requestAnimationFrame(() => {
+      requestAnimationFrame(() => {
+        el.scrollTop = el.scrollHeight;
+      });
+    });
   }
 };
