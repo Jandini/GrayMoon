3a9b9cd8d5b57d48b855c6f086ab1762ba5c95a7
Matt Janda <matt.janda.kingston@gmail.com>
2026-06-13 13:04:33 +0530

    Make the dialog better (prerelease)

--- src/GrayMoon.App/wwwroot/js/ghaLogs.js
+++ src/GrayMoon.App/wwwroot/js/ghaLogs.js
@@ -9,5 +9,19 @@ window.ghaLogs = {
                 window.graymoonShowToast('Copied!');
             } catch (_) { }
         });
+    },
+
+    downloadText: function (filename, content) {
+        const blob = new Blob([content], { type: 'text/plain' });
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = filename;
+        document.body.appendChild(a);
+        a.click();
+        setTimeout(function () {
+            document.body.removeChild(a);
+            URL.revokeObjectURL(url);
+        }, 200);
     }
 };
